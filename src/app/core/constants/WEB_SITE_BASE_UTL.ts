1cdb1985fb6ec4b3c01608fcefac62390b748771
ahmedWessamTest <ahmedwessam202@gmail.com>
2026-07-01 15:49:15 +0200

    feat: add WEB_SITE_BASE_URL and WEB_SITE_IMG_URL constants for environment configuration

--- src/app/core/constants/WEB_SITE_BASE_UTL.ts
+++ src/app/core/constants/WEB_SITE_BASE_UTL.ts
@@ -1,3 +1,9 @@
-const WEB_SITE_BASE_URL = 'https://zisushi.com/zisushitest/api/';
-const WEB_SITE_IMG_URL = 'https://zisushi.com/zisushiv/';
+
+const bases = {
+    production: "https://zisushi.com/zisushitest/api/",
+    testing: "https://zisushi.com/zisushitest-v2/api/",
+};
+
+const WEB_SITE_BASE_URL = bases.testing;
+const WEB_SITE_IMG_URL = "https://zisushi.com/zisushiv/";
 export { WEB_SITE_BASE_URL, WEB_SITE_IMG_URL };
